62ee9912606cbf66dd2729aa56519bfe45c448fb
bruno-gab-galvan <bruno.gab.galvan@gmail.com>
2024-12-13 09:45:17 -0300

    fixes

--- Projects/gcp-caches/raas-gcp/envs/catalog/catalog-web/script.js
+++ Projects/gcp-caches/raas-gcp/envs/catalog/catalog-web/script.js
@@ -1,7 +1,6 @@
 let allData = [];
 let priceData = {};
 let linksData = {};
-
 // Add event listeners for filters
 document.getElementById('region-filter').addEventListener('change', function() {
     filterTable(); // Call filterTable on change without passing arguments
@@ -12,44 +11,34 @@ document.getElementById('type-filter').addEventListener('change', function() {
 document.getElementById('env-filter').addEventListener('change', function() {
     filterTable(); // Call filterTable on change without passing arguments
 });
-
 // Function to filter and generate the table based on the selected filters
 function filterTable() {
     const regionFilter = document.getElementById('region-filter').value.toLowerCase();
     const typeFilter = document.getElementById('type-filter').value.toLowerCase();
     const envFilter = document.getElementById('env-filter').value.toLowerCase();
-
     console.log("Filters - Region: ", regionFilter, " Type: ", typeFilter, " Env: ", envFilter);  // Debugging log
-
     const filteredData = allData.filter(entry => {
         let matches = true;
-
         // Filter out entries where name is 'N/A'
         if (entry.name === 'N/A') {
             matches = false;
         }
-
         // Filter by region
         if (regionFilter && entry.region && !entry.region.toLowerCase().includes(regionFilter)) {
             matches = false;
         }
-
         // Filter by type
         if (typeFilter && entry.type && !entry.type.toLowerCase().includes(typeFilter)) {
             matches = false;
         }
-
         // Filter by env
         if (envFilter && entry.env && !entry.env.toLowerCase().includes(envFilter)) {
             matches = false;
         }
-
         return matches;
     });
-
     generateTable(filteredData, priceData, linksData);  // Re-generate table with filtered data
 }
-
 // Fetch all data
 async function fetchData() {
     try {
@@ -63,23 +52,20 @@ async function fetchData() {
                 })
             )
         );
-
         const fetchedPriceData = await fetch(pricesFile)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Error loading price file ${pricesFile}: ${response.statusText}`);
-                }
-                return response.json(); // Parse the price JSON data
-            });
-
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error loading price file ${pricesFile}: ${response.statusText}`);
+            }
+            return response.json(); // Parse the price JSON data
+        });
         const fetchedLinksData = await fetch(linksFile)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Error loading price file ${linksFile}: ${response.statusText}`);
-                }
-                return response.json(); // Parse the price JSON data
-            });
-
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error loading price file ${linksFile}: ${response.statusText}`);
+            }
+            return response.json(); // Parse the price JSON data
+        });
         allData = dataArrays.flatMap(data => Object.values(data)); // Flatten all the data files
         priceData = fetchedPriceData;
         linksData = fetchedLinksData;
@@ -89,7 +75,6 @@ async function fetchData() {
         console.error('Error fetching data:', error);
     }
 }
-
 // List of all JSON file paths relative to script.js (excluding the new JSON with prices)
 const jsonFiles = [
     'data/prod-europe-west1-memcache.json',
@@ -101,11 +86,9 @@ const jsonFiles = [
     'data/stable-us-central1-memcache.json',
     'data/stable-us-central1-redis.json'
 ];
-
 // Path to the new JSON file with prices
 const pricesFile = 'prices.json';
 const linksFile = 'links.json';
-
 Promise.all([
     // Fetch the other JSON files (without prices)
     Promise.all(
@@ -134,8 +117,7 @@ Promise.all([
         .catch(error => {
             console.error('Error loading price file:', error);
             return {}; // Return an empty object on error
-        }
-    ),
+        }),
     fetch(linksFile)
         .then(response => {
             if (!response.ok) {
@@ -153,16 +135,12 @@ Promise.all([
     allData = dataArrays.flatMap(data => {
         return Object.values(data); // Flatten each file's object into an array of service entries
     });
-
     // Store the price data
     priceData = fetchedPriceData;
     linksData = fetchedLinksData;
-
     // Generate the table with the main data
     generateTable(allData, priceData, linksData);  // Ensure the table is generated initially
 }).catch(error => console.error('Error fetching data:', error));
-
-
 // Function to calculate the Tier according to memory size
 function getNodeType(memory_size) {
     if (memory_size >= 1 && memory_size <= 4) return 'M1';
@@ -171,7 +149,6 @@ function getNodeType(memory_size) {
     if (memory_size >= 36 && memory_size <= 100) return 'M4';
     if (memory_size > 100 && memory_size < 1000) return 'M5';
 }
-
 // Function to calculate the capacity based on node type and shard count
 function getCapNodeType(node_type, shard_count) {
     if (node_type === 'REDIS_SHARED_CORE_NANO') return (shard_count * 1.12).toFixed(2);
@@ -180,57 +157,49 @@ function getCapNodeType(node_type, shard_count) {
     if (node_type === 'REDIS_HIGHMEM_XLARGE') return (shard_count * 46.4).toFixed(2);
     return 'N/A';
 }
-
 function getPricing(priceData, region, type, nodeType, tier, memorySize, shardCount, nodeCount) {
     // Check if the price exists for the given region, type, and tier (Redis Instances)
     if (priceData[region] && priceData[region][type] && priceData[region][type][tier]) {
         return (priceData[region][type][tier] * memorySize * 730).toFixed(2);
     }
-
     // Check for Redis Clusters (by nodeType)
     if (priceData[region] && priceData[region][type] && priceData[region][type][nodeType]) {
         return (priceData[region][type][nodeType] * shardCount * 730).toFixed(2);
     }
-
     // Check for Memcache pricing based on memorySize
     if (priceData[region] && priceData[region][type]) {
-        if ((memorySize / 1024) <= 4) {
-            return (priceData[region][type]['node<=4'] * (memorySize / 1024) * nodeCount * 730).toFixed(2) || 'N/A';  // Fallback if the 'node<=4' pricing doesn't exist priceData[region][type]['node<=4']
+        if (memorySize <= 4) {
+            return (priceData[region][type]['node<=4'] * memorySize * nodeCount * 730).toFixed(2) || 'N/A';  // Fallback if the 'node<=4' pricing doesn't exist priceData[region][type]['node<=4']
         } else {
-            return (priceData[region][type]['node>4'] * (memorySize / 1024) * nodeCount * 730).toFixed(2) || 'N/A';  // Fallback if the 'node>4' pricing doesn't exist
+            return (priceData[region][type]['node>4'] * memorySize * nodeCount * 730).toFixed(2) || 'N/A';  // Fallback if the 'node>4' pricing doesn't exist
         }
     }
-
     // Fallback if no valid pricing is found
     return 'N/A';
 }
-
 function getLink(linksData, env, region, type, name) {
     // Check if the price exists for the given region, type, and tier (Redis Instances)
     if (linksData[env] && linksData[env][region] && linksData[env][region][type] && linksData[env][region][type][name]) {
         return linksData[env][region][type][name];
     }
 }
-
 // Function to generate the table
 function generateTable(data, priceData, linksData) {
     const tableBody = document.getElementById('table-body');
     let rowsHTML = '';
-
     for (const entry of data) {
         const memorySize = entry.memory_size || 0;
         const nodeType = entry.node_type || '';
         const nodeCount = entry.node_count || ''; 
         const type = entry.type || ''; 
-        const region = entry.region || ''; 
+        const region = entry.region || '';
         const env = entry.env || ''; 
-        const name = entry.name || ''; 
+        const name = entry.name || '';  
         const shardCount = entry.shard_count || 0;
-        const node_type_redis_instance = getNodeType(memorySize);
+        const instanceType = getNodeType(memorySize);
         const capacity = getCapNodeType(nodeType, shardCount);
-        const price = getPricing(priceData, region, type, nodeType, node_type_redis_instance, memorySize, shardCount, nodeCount);
+        const price = getPricing(priceData, region, type, nodeType, instanceType, memorySize, shardCount, nodeCount);
         const link = getLink(linksData, env, region, type, name);
-
         rowsHTML += `
             <tr>
                 <td>${entry.name || 'N/A'}</td>
@@ -239,8 +208,8 @@ function generateTable(data, priceData, linksData) {
                 <td>
                     ${entry.service ? `<a href="https://services.groupondev.com/services/${entry.service}" target="_blank">${entry.service}</a>` : 'N/A'}
                 </td>
-                <td>${nodeType || node_type_redis_instance || '-'}</td>
-                <td>${shardCount || nodeCount || 'N/A'}</td>
+                <td>${nodeType || instanceType || '-'}</td>
+                <td>${shardCount || nodeCount || '-'}</td>
                 <td>${memorySize || capacity || 'N/A'}</td>
                 <td>${entry.cache_cname || 'N/A'}</td>
                 <td>
@@ -253,9 +222,7 @@ function generateTable(data, priceData, linksData) {
             </tr>
         `;
     }
-
     tableBody.innerHTML = rowsHTML;
 }
-
 // Fetch and load data
 fetchData();
